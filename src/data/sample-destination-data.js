ccd5a6f609dce59d641cb3b807dd3c55b7888434
Jeffrey Cook <cookturingedu@gmail.com>
2022-09-24 21:41:29 -0400

    Add sample data to sample-destinations-data file

---
+++ src/data/sample-destination-data.js
@@ -0,0 +1,28 @@
+const destinations = [
+  {
+    id: 41,
+    destination: "Montego Bay, Jamaica",
+    estimatedLodgingCostPerDay: 500,
+    estimatedFlightCostPerPerson: 100,
+    image: "https://images.unsplash.com/photo-1557129604-0e50f1300fab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    alt: "boats docked beside trees on river"
+  },
+  {
+    id: 34,
+    destination: "Seoul, South Korea",
+    estimatedLodgingCostPerDay: 1200,
+    estimatedFlightCostPerPerson: 150,
+    image: "https://images.unsplash.com/photo-1578193661644-dee2e67b779b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2025&q=80",
+    alt: "aerial photography of city during daytime"
+  },
+  {
+    id: 18,
+    destination: "Cape Town, South Africa",
+    estimatedLodgingCostPerDay: 120,
+    estimatedFlightCostPerPerson: 1200,
+    image: "https://images.unsplash.com/photo-1522576775862-7168ae29372c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1568&q=80",
+    alt: "a city with mountain cliffs by the sea"
+  }
+]
+
+export default destinations;
