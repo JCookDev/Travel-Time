172dd22c99ba2eedc8ffd050a41042be1da6d12f
Jeffrey Cook <cookturingedu@gmail.com>
2022-09-24 21:28:49 -0400

    Add properties and method to Traveler class

---
+++ src/Traveler.js
@@ -0,0 +1,13 @@
+class Traveler {
+  constructor(travelerData) {
+    this.id = travelerData.id;
+    this.name = travelerData.name;
+    this.travelerType = travelerData.travelerType;
+  }
+  returnFirstName = () => {
+    const firstName = this.name.split(" ");
+    return firstName[0];
+  };
+}
+
+export default Traveler;
