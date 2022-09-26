131f94c9e0baed4f3712047a43fa4522a2c975a7
Jeffrey Cook <cookturingedu@gmail.com>
2022-09-26 18:21:10 -0400

    Fetch traveler info from api, generate random traveler on page load and display greet message

--- src/scripts.js
+++ src/scripts.js
@@ -24,4 +24,35 @@ let travelerRepo, tripRepo, destinationRepo;
 let currentTraveler;
 let travelerInput;
 
+//Functions
+const fetchApiCalls = () => {
+  apiCalls.fetchData().then(data => {
+    let travelerData = data[0].travelers;
+    let tripData = data[1].trips;
+    let destinationData = data[2].destinations;
+    currentTraveler = new Traveler(travelerData[getRandomIndex(travelerData)])
+    travelerRepo = new TravelerRepo(travelerData);
+    travelerRepo.instantiateTraveler();
+    tripRepo = new TripRepo(tripData);
+    tripRepo.instantiateTrips();
+    destinationRepo = new DestinationRepo(destinationData);
+    destinationRepo.instantiateDestination();
+    tripRepo.filterTripsByTraveler(currentTraveler.id);
+    loadPage();
+  });
+};
+
+const loadPage = () => {
+  greetTraveler();
+};
+
+const greetTraveler = () => {
+  welcomeMessage.innerHTML = `Welcome back, ${currentTraveler.returnFirstName()}!`;
+};
+
+const getRandomIndex = (array) => {
+  return Math.floor(Math.random() * array.length);
+}
+
+window.addEventListener("load", fetchApiCalls());
 console.log('This is the JavaScript entry file - your code begins here.');
