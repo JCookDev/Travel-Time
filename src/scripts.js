f4abed4377971ecc3623fbc773b641f299ff5677
Jeffrey Cook <cookturingedu@gmail.com>
2022-09-26 17:08:27 -0400

    Add query selector to trip cards

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,15 @@ import Traveler from "../src/Traveler";
 
 const dayjs = require("dayjs");
 
-
+// Query Selectors
+const welcomeMessage = document.querySelector("#welcomeMessage");
+const destinationsDropDown = document.querySelector("#destinationsDropDown");
+const bookingDateInput = document.querySelector("#bookingDateInput");
+const durationInput = document.querySelector("#durationInput");
+const guestsInput = document.querySelector("#guestsInput");
+const estimateButton = document.querySelector("#estimateButton");
+const submitButton = document.querySelector("#submitButton");
+const estimateMessage = document.querySelector("#estimateMessage");
+const tripCards = document.querySelector(".trip-cards");
 
 console.log('This is the JavaScript entry file - your code begins here.');
