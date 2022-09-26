68825aa22bef75729d5b0735295311ef907e86a8
Jeffrey Cook <cookturingedu@gmail.com>
2022-09-26 17:18:07 -0400

    Add fetch traveler, destinations and trip data from api

---
+++ src/apiCalls.js
@@ -0,0 +1,17 @@
+const fetchApiData = type => {
+  return (
+    fetch(`http://localhost:3001/api/v1/${type}`)
+      .then(response => response.json())
+      .catch(error => console.log(`${type} API error!`))
+  );
+};
+
+const fetchData = () => {
+  return Promise.all([
+    fetchApiData("travelers"),
+    fetchApiData("trips"),
+    fetchApiData("destinations")
+  ]);
+};
+
+export default { fetchData };
