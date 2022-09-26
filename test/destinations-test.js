0976730d1ae65026ef21d16fb36ab165c0fdc3c2
Jeffrey Cook <cookturingedu@gmail.com>
2022-09-25 22:45:06 -0400

    Add properties to Destination class and pass all tests

--- test/destinations-test.js
+++ test/destinations-test.js
@@ -1,6 +1,6 @@
 import chai from "chai";
 const expect = chai.expect;
-//import Destination from "../src/Destinations";
+import Destination from "../src/Destinations";
 import destinations from "../src/data/sample-destination-data";
 
 describe("Destination", () => {
@@ -11,36 +11,36 @@ describe("Destination", () => {
     destination2 = new Destination(destinations[2]);
   });
 
-  it.skip("should be a function", () => {
+  it("should be a function", () => {
     expect(Destination).to.be.a("function");
   });
 
-  it.skip("should instantiate a destination", () => {
+  it("should instantiate a destination", () => {
     expect(destination1).to.be.an.instanceof(Destination);
     expect(destination2).to.be.an.instanceOf(Destination);
   });
 
-  it.skip("should have a destination id", () => {
+  it("should have a destination id", () => {
     expect(destination1.id).to.equal(41);
     expect(destination2.id).to.equal(18);
   });
 
-  it.skip("should have a destination name", () => {
+  it("should have a destination name", () => {
     expect(destination1.destination).to.equal("Montego Bay, Jamaica");
     expect(destination2.destination).to.equal("Cape Town, South Africa");
   });
 
-  it.skip("should have an estimated lodging cost per day", () => {
+  it("should have an estimated lodging cost per day", () => {
     expect(destination1.estimatedLodgingCostPerDay).to.equal(500);
     expect(destination2.estimatedLodgingCostPerDay).to.equal(120);
   });
 
-  it.skip("should have an estimated flight cost per person", () => {
+  it("should have an estimated flight cost per person", () => {
     expect(destination1.estimatedFlightCostPerPerson).to.equal(100);
     expect(destination2.estimatedFlightCostPerPerson).to.equal(1200);
   });
 
-  it.skip("should have an image of destination", () => {
+  it("should have an image of destination", () => {
     expect(destination1.image).to.equal(
       "https://images.unsplash.com/photo-1557129604-0e50f1300fab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
     );
@@ -49,7 +49,7 @@ describe("Destination", () => {
     );
   });
 
-  it.skip("should have an alt description for destination image", () => {
+  it("should have an alt description for destination image", () => {
     expect(destination1.alt).to.equal(
       "boats docked beside trees on river"
     );
