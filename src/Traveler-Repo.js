2ac73f4bd186b4cd083edfe4ba21f13b2df82ccc
Jeffrey Cook <cookturingedu@gmail.com>
2022-09-26 14:01:06 -0400

    Update typo errors, add properties and methods to travelors repo class and pass all tests

---
+++ src/Traveler-Repo.js
@@ -0,0 +1,21 @@
+import Traveler from "../src/Traveler";
+
+class TravelerRepo {
+  constructor(data) {
+    this.data = data;
+  }
+
+  instantiateTraveler = () => {
+    return (this.data = this.data.map(traveler => {
+      return new Traveler(traveler);
+    }));
+  };
+
+  findCurrentTraveler = id => {
+    return this.data.find(traveler => {
+      return traveler.id === id;
+    });
+  };
+}
+
+export default TravelerRepo;
--- test/traveler-repo-test.js
+++ test/traveler-repo-test.js
@@ -2,7 +2,7 @@ import chai from "chai";
 const expect = chai.expect;
 
 import Traveler from "../src/Traveler";
-//import TravelerRepo from "../src/traveler-repo";
+import TravelerRepo from "../src/Traveler-Repo";
 
 describe("TravelerRepo", () => {
   let traveler1;
@@ -23,25 +23,25 @@ describe("TravelerRepo", () => {
     travelerRepo = new TravelerRepo([traveler1, traveler2]);
   });
 
-  it.skip("should be a function", () => {
+  it("should be a function", () => {
     expect(TravelerRepo).to.be.a("function");
   });
 
-  it.skip("should be an instance of TravelerRepo", () => {
+  it("should be an instance of TravelerRepo", () => {
     expect(travelerRepo).to.be.an.instanceof(TravelerRepo);
   });
 
-  it.skip("should store data for multiple travelers", () => {
+  it("should store data for multiple travelers", () => {
     expect(travelerRepo.data).to.deep.equal([traveler1, traveler2]);
   });
 
-  it.skip("should have multiple instances of Traveler", () => {
+  it("should have multiple instances of Traveler", () => {
     travelerRepo.instantiateTraveler();
     expect(travelerRepo.data[0]).to.be.an.instanceOf(Traveler);
     expect(travelerRepo.data[1]).to.be.an.instanceOf(Traveler);
   });
 
-  it.skip("should find traveler by id", () => {
+  it("should find traveler by id", () => {
     const selectedTraveler1 = travelerRepo.findCurrentTraveler(traveler1.id);
     expect(selectedTraveler1.id).to.equal(traveler1.id);
   });
